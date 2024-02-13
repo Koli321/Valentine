c0d472b60625a363d576335daea2ef675a091cd4
Koli321 <94520383+Koli321@users.noreply.github.com>
2024-02-14 00:19:17 +0500

    add

---
+++ script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+  $('.title').click(function(){
+    $('.container').addClass('open');
+  });
+  
+  
+  $('.close').click(function(){
+    $('.container').removeClass('open');
+  });
+});
+
+function flipCover (css, options) {
+  var options = options || {}
+  if (typeof css === "object") {
+    options = css
+  } else {
+    options.css = css
+  }
+
+  var css = options.css
+  var url = options.url
+  var text = options.text || css
+  var width = options.width
+  var height = options.height
+
+  var $section = $(".flip-cover-" + css).addClass(css + "-section")
+  var $button = $("<div>").addClass(css + "-button")
+  var $cover = $("<div>").addClass(css + "-cover")
+  var $outer = $("<div>").addClass(css + "-outer")
+  var $inner = $("<div>").addClass(css + "-inner")
+
+  if (width) {
+    $section.css("width", width)
+  }
+
+  if (height) {
+    $section.css("height", height)
+
+    var lineHeight = ':after{ line-height: ' + height + ';}'
+    var $outerStyle = $('<style>').text('.' + css + '-outer' + lineHeight)
+    $outerStyle.appendTo($outer)
+    var $innerStyle = $('<style>').text('.' + css + '-inner' + lineHeight)
+    $innerStyle.appendTo($inner)
+  }
+
+  $cover.html($outer)
+  $inner.insertAfter($outer)
+
+  $button.html($("<a>").text(text).attr("href", url))
+
+  $section.html($button)
+  $cover.insertAfter($button)
+ }
+
+
+
+flipCover({
+  css: "dribbble",
+  text: "Ты чо жопа?",
+  width: "80px"
+})
+
+flipCover("twiter", {
+  css: "dribbble",
+  url: "file:///C:/Users/mrlor/Desktop/%D0%A1%D0%B2%D0%B8%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5/dist/index.html",
+  text: "Хихи",
+  width: "80px"
+})
+
