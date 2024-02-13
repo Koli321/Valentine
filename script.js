06bb6609cef562c4a588eed0f5442ca0ec279f4e
Koli321 <94520383+Koli321@users.noreply.github.com>
2024-02-14 00:26:39 +0500

    chance url

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ flipCover({
 
 flipCover("twiter", {
   css: "dribbble",
-  url: "file:///C:/Users/mrlor/Desktop/%D0%A1%D0%B2%D0%B8%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5/dist/index.html",
+  url: "/dist/index.html",
   text: "Хихи",
   width: "80px"
 })
